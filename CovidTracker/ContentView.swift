78f8984d59c89ca49222c480bc4cab772c1a3b02
Hakim Laoukili <hakimlaoukili@gmail.com>
2020-12-16 16:35:24 +0100

    Initial Commit

---
+++ CovidTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidTracker
+//
+//  Created by Hakim Laoukili on 2020-12-16.
+//  Copyright © 2020 Hakim Laoukili. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
